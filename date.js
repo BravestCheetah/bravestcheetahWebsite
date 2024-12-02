d000e363005c0bb4073ff0683214f4390f826de5
BravestCheetah <158742917+BravestCheetah@users.noreply.github.com>
2024-12-02 09:45:19 +0100

    adding date show cool yes

---
+++ date.js
@@ -0,0 +1,17 @@
+document.getElementById('currentTime').textContent = '(LOADING)';
+
+setInterval(function() {
+  var currentDate = new Date();
+  
+  var year = currentDate.getFullYear();
+  var month = currentDate.getMonth() + 1;
+  var day = currentDate.getDay() + 1;
+  
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+  
+  var currentTime = hours + ":" + minutes + ":" + seconds
+  var currentDay = day + "/" + month + "/" + year
+  document.getElementById('currentTime').textContent = currentDay + " : " + currentTime;
+}, 1000)
