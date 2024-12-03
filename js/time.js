7aafd6b0cd138a44ea493796f3d397ad4414d998
BravestCheetah <158742917+BravestCheetah@users.noreply.github.com>
2024-12-03 14:10:57 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -11,6 +11,17 @@ setInterval(function() {
   var minutes = currentDate.getMinutes();
   var seconds = currentDate.getSeconds();
   
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  
   var currentTime = hours + ":" + minutes + ":" + seconds
   var currentDay = day + "/" + month + "/" + year
   document.getElementById('currentTime').textContent = currentDay + " : " + currentTime;
