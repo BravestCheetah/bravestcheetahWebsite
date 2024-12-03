b48cf2ccae3b8a8c3336a7b5f32655215b5c4c7b
BravestCheetah <158742917+BravestCheetah@users.noreply.github.com>
2024-12-03 10:24:57 +0100

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+setInterval(function() {
+  var currentDate = new Date();
+  
+  var year = currentDate.getFullYear();
+  var month = currentDate.getMonth() + 1;
+  var day = currentDate.getDay() + 1;
+  
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+  
+  var currentTime = hours + ":" + minutes + ":" + seconds
+  var currentDay = day + "/" + month + "/" + year
+  document.getElementById('clock').textContent = currentTime;
+  document.getElementById('date').textContent = currentDay;
+}, 1000)
