682f30306b654611344c27e053c4bbc5bc5dda18
BravestCheetah <158742917+BravestCheetah@users.noreply.github.com>
2024-12-10 14:25:14 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -5,7 +5,7 @@ setInterval(function() {
   
   var year = currentDate.getFullYear();
   var month = currentDate.getMonth() + 1;
-  var day = currentDate.getDay();
+  var day = currentDate.getDate() + 1;
   
   var hours = currentDate.getHours();
   var minutes = currentDate.getMinutes();
